05eaac584ba771424402dc730adb3f0aa8d611de
TallesMR <tallesrodrigues@gmail.com>
2023-02-07 19:26:12 -0300

    iniciando projetos

--- src/app/projetos/projetos.component.ts
+++ src/app/projetos/projetos.component.ts
@@ -12,4 +12,22 @@ export class ProjetosComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  showText(project: string){
+    var display = document.getElementById(project)!.style.display;
+    display == "none" ? document.getElementById(project)!.style.display = "block" : document.getElementById(project)!.style.display = "none";
+  }
+
+  expandImage(project:string){
+    let imagem = document.getElementById(project)!.style;
+    imagem.cssText = 'opacity: 100%; max-width: 1300px; height: 600px; object-fit: cover;margin-left: 1000px;'
+    document.getElementById('olho')!.style.display = "none"
+  }
+
+  retractImage(project: string){
+  let imagem = document.getElementById(project)!.style; 
+  document.getElementById('olho')!.style.display = "block";
+  
+  imagem.cssText = 'align-items: center; max-width: 350px; height: 300px; opacity: 80%;'
+  }
+
 }
